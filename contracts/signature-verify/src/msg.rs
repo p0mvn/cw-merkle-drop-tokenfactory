2427acc329573136fe43c41b818a6dca3422656e
Roman Akhtariev <roman@akhtariev.localdomain>
2022-08-30 22:53:03 -0400

    initial commit

---
+++ contracts/signature-verify/src/msg.rs
@@ -0,0 +1,30 @@
+use std::string;
+
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub count: i32,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    Increment {},
+    Reset { count: i32 },
+    LazyMint { account: Vec<u8>, signature: Vec<u8> }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    // GetCount returns the current count as a json-encoded number
+    GetCount {},
+}
+
+// We define a custom struct for each query response
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct GetCountResponse {
+    pub count: i32,
+}
---
+++ ts/sdk/README.md
@@ -0,0 +1,1 @@
+# merkle-drop-app-sdk
