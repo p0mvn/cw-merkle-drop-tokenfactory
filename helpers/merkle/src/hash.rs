cdab222c48761d03a9bcb0547b0d8d82de10f02d
Roman <roman@osmosis.team>
2022-09-11 18:58:26 -0400

    custom serialization on hash

--- helpers/merkle/src/hash.rs
+++ helpers/merkle/src/hash.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-use serde::{Deserialize, Serialize};
+use serde::{de, Deserialize, Serialize, Serializer, Deserializer, de::Visitor};
 use sha3::{Digest, Sha3_256};
 
 // The distinction in prefixes is needed
@@ -12,8 +12,48 @@ const BRANCH_NODE_PREFIX: &[u8] = &[1];
 
 pub const HASH_BYTES: usize = 32;
 
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Serialize, Deserialize, Default)]
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Default)]
 pub struct Hash(pub(crate) [u8; HASH_BYTES]);
+
+// Serialize for Entry is the custom serialization implementation.
+// Since plain-text SHA3 hash might not be exclusive to the ASCII set,
+// we need to first base64 encoded it before serializing. This is what
+// this implementation achieves.
+impl Serialize for Hash {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let hash = base64::encode(self);
+        serializer.serialize_bytes(hash.as_bytes())
+    }
+}
+
+struct HashVisitor;
+
+impl<'de> Visitor<'de> for HashVisitor {
+    type Value = Hash;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("an integer between -2^31 and 2^31")
+    }
+
+    fn visit_byte_buf<E>(self, v: Vec<u8>) -> Result<Self::Value, E>
+        where
+            E: de::Error, {
+        let hash = Hash::from(v);
+        Ok(hash)
+    }
+}
+
+impl<'de> Deserialize<'de> for Hash {
+    fn deserialize<D>(deserializer: D) -> Result<Hash, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_byte_buf(HashVisitor)
+    }
+}
 
 impl fmt::Display for Hash {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -68,4 +108,20 @@ fn hash(values: &[&[u8]]) -> Hash {
         hasher.update(value);
     }
     hasher.result()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_util;
+
+    #[test]
+    fn custom_serialization_works() {
+        let test_entry = leaf(test_util::OSMO);
+        
+        let serialized = serde_json::to_string(&test_entry).unwrap();
+        let deserialized: Hash = serde_json::from_str(&serialized).unwrap();
+
+        assert_eq!(test_entry, deserialized);
+    }
 }
--- helpers/merkle/src/proof.rs
+++ helpers/merkle/src/proof.rs
@@ -1,13 +1,7 @@
-use std::fmt;
-
 use crate::hash;
-use ::serde::de::{Deserializer, MapAccess, SeqAccess, Visitor};
-use base64;
-use serde;
-use serde::{de, ser::SerializeStruct, Deserialize, Serialize, Serializer};
-use std::error::Error;
+use serde::{Deserialize, Serialize};
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Serialize, Deserialize)]
 pub struct Entry {
     pub is_left_sibling: bool,
     pub hash: hash::Hash,
@@ -21,122 +15,6 @@ impl Entry {
         };
         entry
     }
-}
-
-// Serialize for Entry is the custom serialization implementation.
-// Since plain-text SHA3 hash might not be exclusive to the ASCII set,
-// we need to first base64 encoded it before serializing. This is what
-// this implementation achieves.
-impl Serialize for Entry {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut state = serializer.serialize_struct("Entry", 2)?;
-        state.serialize_field("is_left_sibling", &self.is_left_sibling)?;
-        let encoded_hash = base64::encode(self.hash);
-        state.serialize_field("hash", &encoded_hash)?;
-        state.end()
-    }
-}
-
-// Deserialize for Entry is the custom deserialization implementation.
-// Since plain-text SHA3 hash might not be exclusive to the ASCII set,
-// we need to first base64 encoded it before serializing. This
-// implementation assumes that the serialized hash is base64 encoded.
-// As a result, it decodes it first before creating an Entry.
-impl<'de> Deserialize<'de> for Entry {
-    fn deserialize<D>(deserializer: D) -> Result<Entry, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        #[derive(Deserialize)]
-        #[serde(field_identifier, rename_all = "lowercase")]
-        enum Field {
-            IsLeftSibling,
-            Base64Hash,
-        }
-
-        struct EntryVisitor;
-
-        impl<'de> Visitor<'de> for EntryVisitor {
-            type Value = Entry;
-
-            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                formatter.write_str("struct Duration")
-            }
-
-            fn visit_seq<V>(self, mut seq: V) -> Result<Entry, V::Error>
-            where
-                V: SeqAccess<'de>,
-            {
-                let is_left_sibling = seq
-                    .next_element()?
-                    .ok_or_else(|| de::Error::invalid_length(0, &self))?;
-                let base64_hash: String = seq
-                    .next_element()?
-                    .ok_or_else(|| de::Error::invalid_length(1, &self))?;
-
-                let decoded_result = base64_to_hash(base64_hash);
-
-                if decoded_result.is_err() {
-                    return Err(de::Error::custom("hash was not base64 encoded"));
-                }
-
-                Ok(Entry::new(
-                    is_left_sibling,
-                    hash::Hash::from(decoded_result.unwrap()),
-                ))
-            }
-
-            fn visit_map<V>(self, mut map: V) -> Result<Entry, V::Error>
-            where
-                V: MapAccess<'de>,
-            {
-                let mut is_left_sibling_opt = None;
-                let mut base64_hash_opt: Option<String> = None;
-                while let Some(key) = map.next_key()? {
-                    match key {
-                        Field::IsLeftSibling => {
-                            if is_left_sibling_opt.is_some() {
-                                return Err(de::Error::duplicate_field("is_left_sibling"));
-                            }
-                            is_left_sibling_opt = Some(map.next_value()?);
-                        }
-                        Field::Base64Hash => {
-                            if base64_hash_opt.is_some() {
-                                return Err(de::Error::duplicate_field("hash"));
-                            }
-                            base64_hash_opt = Some(map.next_value()?);
-                        }
-                    }
-                }
-                let is_left_sibling = is_left_sibling_opt
-                    .ok_or_else(|| de::Error::missing_field("is_left_sibling"))?;
-                let base64_hash =
-                    base64_hash_opt.ok_or_else(|| de::Error::missing_field("hash"))?;
-
-                let decoded_result = base64_to_hash(base64_hash);
-
-                if decoded_result.is_err() {
-                    return Err(de::Error::custom("hash was not base64 encoded"));
-                }
-
-                Ok(Entry::new(
-                    is_left_sibling,
-                    hash::Hash::from(decoded_result.unwrap()),
-                ))
-            }
-        }
-
-        const FIELDS: &'static [&'static str] = &["is_left_sibling", "hash"];
-        deserializer.deserialize_struct("Entry", FIELDS, EntryVisitor)
-    }
-}
-
-fn base64_to_hash(base64: String) -> Result<hash::Hash, Box<dyn Error>> {
-    let decoded_result = base64::decode(base64)?;
-    Ok(hash::Hash::from(decoded_result))
 }
 
 #[derive(Default, Serialize, Deserialize)]
