402f36fb11cc7d2c8adfe90d76003eb336b36f27
Roman <roman@osmosis.team>
2022-10-01 21:28:31 -0400

    begin trying osmosis-testing

---
+++ contracts/merkle-drop/tests/claim.rs
@@ -0,0 +1,127 @@
+mod test_env;
+use cosmwasm_std::{Coin, Uint128};
+use osmosis_std::types::osmosis::gamm::v1beta1::SwapAmountInRoute;
+use osmosis_testing::{Module, RunnerError, Wasm};
+use merkle_drop::msg::{ExecuteMsg, QueryMsg};
+use test_env::*;
+
+const TEST_ROOT: &str = "Nz54SQtyBVHwsmEqNI//mxFgiq8MRD7sS92IGkhgMvo=";
+
+const VALID_PROOF_STR: &str = "[{\"is_left_sibling\":true,\"hash\":[77,122,52,81,83,57,77,56,52,118,78,84,89,115,86,76,120,80,121,50,109,89,49,111,107,85,111,68,111,108,97,118,105,81,119,71,114,54,111,49,51,107,99,61]},{\"is_left_sibling\":true,\"hash\":[113,53,102,108,80,98,70,114,73,121,100,108,105,97,121,52,85,73,57,100,107,111,106,71,82,116,49,57,90,71,121,105,51,111,55,104,103,112,87,103,73,117,48,61]},{\"is_left_sibling\":true,\"hash\":[114,87,52,69,82,120,75,113,110,53,102,77,69,65,114,57,56,118,90,80,116,54,119,108,67,71,49,69,80,121,99,57,51,54,108,48,112,100,86,97,100,120,48,61]},{\"is_left_sibling\":false,\"hash\":[66,86,43,54,113,43,104,43,100,99,115,116,50,83,66,52,122,68,111,100,118,50,98,90,84,112,108,105,105,65,104,87,50,82,75,121,112,67,72,51,81,66,85,61]},{\"is_left_sibling\":false,\"hash\":[86,100,75,79,107,112,115,120,78,73,103,43,50,111,102,47,57,76,84,111,122,51,107,84,102,53,112,90,113,81,108,108,112,86,84,86,82,55,108,77,103,83,119,61]}]";
+
+test_claim!(
+    claim
+    should succeed,
+
+    
+    sender = Owner,
+    msg = ExecuteMsg::Claim {
+        proof: String::from(VALID_PROOF_STR),
+        amount: Coin { denom: String::from("uosmo"), amount: Uint128::from(503 as u128) }
+    },
+    claimer_addr: &String::from("osmo1000xz25ydz8h9rwgnv30l9p0x500dvj0wv50ft")
+);
+
+// test_set_route!(
+//     set_initial_route_by_non_owner
+//     should failed_with "Unauthorized: execute wasm contract failed",
+
+//     sender = NonOwner,
+//     msg = ExecuteMsg::SetRoute {
+//         input_denom: "uosmo".to_string(),
+//         output_denom: "uion".to_string(),
+//         pool_route: vec![SwapAmountInRoute {
+//             pool_id: 1,
+//             token_out_denom: "uion".to_string(),
+//         }],
+//     }
+// );
+
+// ======= helpers ========
+
+#[macro_export]
+macro_rules! test_claim {
+    ($test_name:ident should succeed, sender = Owner, msg = $msg:expr, claimer_addr: $claimer_addr:expr) => {
+        #[test]
+        fn $test_name() {
+            test_set_route_success_case($msg, $claimer_addr)
+        }
+    };
+
+    ($test_name:ident should failed_with $err:expr, sender = $sender:ident, msg = $msg:expr) => {
+        #[test]
+        fn $test_name() {
+            test_set_route_failed_case(Sender::$sender, $msg, $err)
+        }
+    };
+}
+
+enum Sender {
+    Owner,
+    NonOwner,
+}
+
+fn test_set_route_success_case(msg: ExecuteMsg, claimer_addr: &str) {
+    let TestEnv {
+        app,
+        contract_address,
+        owner,
+    } = TestEnv::new();
+
+    
+
+    let wasm = Wasm::new(&app);
+    let res = wasm.execute(&contract_address, &msg, &[], &owner);
+
+    // check if execution succeeded
+    assert!(res.is_ok(), "{:?}", res.unwrap_err());
+
+    // check if previously set route can be queried correctly
+    match msg {
+        ExecuteMsg::Claim {
+            proof,
+            amount,
+            ..
+        } => {
+            // validate that minted and sent to claimer
+        }
+        _ => {
+            panic!("ExecuteMsg must be `SetRoute`");
+        }
+    }
+}
+
+// fn test_set_route_failed_case(sender: Sender, msg: ExecuteMsg, expected_error: &str) {
+//     let TestEnv {
+//         app,
+//         contract_address,
+//         owner,
+//     } = TestEnv::new();
+//     let wasm = Wasm::new(&app);
+
+//     let sender = match sender {
+//         Sender::Owner => owner,
+//         Sender::NonOwner => {
+//             let initial_balance = [
+//                 Coin::new(1_000_000_000_000, "uosmo"),
+//                 Coin::new(1_000_000_000_000, "uion"),
+//                 Coin::new(1_000_000_000_000, "stake"),
+//             ];
+//             app.init_account(&initial_balance).unwrap()
+//         }
+//     };
+
+//     let res = wasm.execute::<ExecuteMsg>(&contract_address, &msg, &[], &sender);
+//     let err = res.unwrap_err();
+
+//     // assert on error message
+//     if let RunnerError::ExecuteError { msg } = &err {
+//         let expected_err = &format!(
+//             "failed to execute message; message index: 0: {}",
+//             expected_error
+//         );
+//         assert_eq!(msg, expected_err);
+//     } else {
+//         panic!("unexpected error: {:?}", err);
+//     }
+// }
---
+++ contracts/merkle-drop/tests/test_env.rs
@@ -0,0 +1,65 @@
+use std::path::PathBuf;
+
+use cosmwasm_std::Coin;
+use osmosis_testing::{Account, OsmosisTestApp, SigningAccount};
+use osmosis_testing::{Gamm, Module, Wasm};
+use merkle_drop::msg::InstantiateMsg;
+
+pub struct TestEnv {
+    pub app: OsmosisTestApp,
+    pub contract_address: String,
+    pub owner: SigningAccount,
+}
+impl TestEnv {
+    pub fn new() -> Self {
+        let app = OsmosisTestApp::new();
+        let gamm = Gamm::new(&app);
+        let wasm = Wasm::new(&app);
+
+        // setup owner account
+        let initial_balance = [
+            Coin::new(1_000_000_000_000, "uosmo"),
+            Coin::new(1_000_000_000_000, "uion"),
+            Coin::new(1_000_000_000_000, "stake"),
+        ];
+        let owner = app.init_account(&initial_balance).unwrap();
+
+        let code_id = wasm
+            .store_code(&get_wasm(), None, &owner)
+            .unwrap()
+            .data
+            .code_id;
+
+        let contract_address = wasm
+            .instantiate(
+                code_id,
+                &InstantiateMsg {
+                    merkle_root: String::from("",),
+                },
+                Some(&owner.address()),
+                None,
+                &[],
+                &owner,
+            )
+            .unwrap()
+            .data
+            .address;
+
+        TestEnv {
+            app,
+            contract_address,
+            owner,
+        }
+    }
+}
+
+fn get_wasm() -> Vec<u8> {
+    let wasm_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("..")
+        .join("..")
+        .join("target")
+        .join("wasm32-unknown-unknown")
+        .join("release")
+        .join("merkle_drop.wasm");
+    std::fs::read(wasm_path).unwrap()
+}
